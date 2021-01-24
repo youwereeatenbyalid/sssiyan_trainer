d8b7e6e74d4bfdfcc7f7d7ce03e9764b16b851c6
Siyan <gsssiyan@gmail.com>
2021-01-24 14:19:53 +0000

    Make damage toggles only work when cheat is ticked
    Needs testing in pvp, kept old version just in case

--- src/mods/AllOrNothing.cpp
+++ src/mods/AllOrNothing.cpp
@@ -22,6 +22,136 @@ static naked void detour() {
 
 	validation:
         cmp byte ptr [noonetakesdamage], 1
+        je nodamagecheck
+
+    cheatcode:
+        //if enemy, jump to allornothing
+        cmp byte ptr [r8+0xF4], 1
+        je allornothing
+        //we pvp bois?
+        cmp byte ptr [HeavyDay::cheaton], 1
+        jne originalcode
+        //if networked enemy don't do damage
+        cmp byte ptr [r8+0xF4], 3
+        je nodamage
+
+        //if it's the master aka player character, multiply incoming damage
+        //because players have stupid amounts of health.
+        cmp byte ptr [r8+0xF4], 2
+        je playerdamage
+
+        //idk why you'd be here lol
+        jmp originalcode
+    allornothing:
+    //Raw IK?
+    cmp byte ptr [onehitkill], 1
+    je alldamage
+    //if we're not in combat, jump to the original code
+	cmp byte ptr [AllOrNothing::cheaton], 0
+    je originalcode
+    cmp byte ptr [PlayerTracker::incombat], 0
+    je originalcode
+    //don't need the enemy entity anymore, so move the style bar in
+    //style bar is the style rank we must equal or be above to do damage
+    mov r8, [AllOrNothing::stylebar]
+    cmp byte ptr [StyleRank::rank],r8b
+    //if below, do no damage to the enemy.
+    jb nodamagecheck
+    jmp originalcode
+
+    nodamagecheck: // only check for cheaton if sent to nodamage via damage cheats. Skip this if PVP.
+        cmp byte ptr [AllOrNothing::cheaton], 1
+        je nodamage
+        jmp originalcode
+
+    nodamage:
+        //makes the damage done 0
+        mulss xmm6, [zerovalue]
+        jmp originalcode
+    playerdamage:
+        //player multiplier for PVP damage.
+        mulss xmm6, [playermult]
+        jmp originalcode
+    alldamage:
+        //this makes the damage done = the enemies current health.
+        cmp byte ptr [AllOrNothing::cheaton], 0 // Alldamage isn't used in PVP, so can safely limit this to AllOrNothing cheat
+        je originalcode
+        movss xmm6, [rdi+0x10]
+        jmp originalcode
+    originalcode:
+        pop r8
+        movss xmm1,[rdi+0x10]
+        jmp qword ptr [AllOrNothing::jmp_ret]
+	}
+}
+
+// clang-format on
+
+std::optional<std::string> AllOrNothing::on_initialize() {
+  auto base = g_framework->get_module().as<HMODULE>(); // note HMODULE
+  onpage    = gamepage;
+  ischecked = &AllOrNothing::cheaton;
+
+  full_name_string     = "Must Style / Damage Toggles (+)";
+  author_string        = "The Hitchhiker";
+  description_string   = "Disable damage altogether or when below a certain Style Rank.";
+
+  auto addr = utility::scan(base, "F3 0F 10 4F 10 0F 57 C0 0F 5A");
+  if (!addr) {
+    return "Unable to find AllOrNothing pattern.";
+  }
+  if (!install_hook_absolute(addr.value(), m_function_hook, &detour, &jmp_ret, 5)) {
+  //  return a error string in case something goes wrong
+    spdlog::error("[{}] failed to initialize", get_name());
+    return "Failed to initialize AllOrNothing";
+  }
+  return Mod::on_initialize();
+}
+
+void AllOrNothing::on_config_load(const utility::Config& cfg) {
+  noonetakesdamage = cfg.get<bool>("no_one_takes_damage").value_or(false);
+  onehitkill       = cfg.get<bool>("one_hit_kill").value_or(false);
+  AllOrNothing::stylebar = cfg.get<int>("style_damage_requirement").value_or(5);
+}
+void AllOrNothing::on_config_save(utility::Config& cfg) {
+  cfg.set<bool>("no_one_takes_damage", noonetakesdamage);
+  cfg.set<bool>("one_hit_kill", onehitkill);
+  cfg.set<int>("style_damage_requirement", AllOrNothing::stylebar);
+}
+
+void AllOrNothing::on_draw_ui() {
+  ImGui::Checkbox("No one takes damage", &noonetakesdamage);
+  ImGui::Checkbox("One hit kill", &onehitkill);
+  ImGui::Text("Style Rank to beat (1:D, 7:SSS)");
+  ImGui::SliderInt("##StlyeRankRequirement", (int*)&AllOrNothing::stylebar, 1, 7);
+  
+}
+
+#if 0 // version before siyan attacked; cheats work even without AllOrNothing cheaton bool
+#include "AllOrNothing.hpp"
+#include "mods/PlayerTracker.hpp"
+#include "mods/HeavyDay.hpp"
+#include "mods/StyleRank.hpp"
+uintptr_t AllOrNothing::jmp_ret{NULL};
+bool AllOrNothing::cheaton{NULL};
+uint32_t AllOrNothing::stylebar{NULL};
+bool noonetakesdamage;
+bool onehitkill;
+float zerovalue = 0.0;
+float playermult = 10.0;
+// clang-format off
+// only in clang/icl mode on x64, sorry
+
+static naked void detour() {
+	__asm {
+    newmem:
+        push r8
+        mov r8, [r14+0x98]
+        test r8, r8
+        je originalcode
+
+	validation:
+        cmp byte ptr [noonetakesdamage], 1
         je nodamage
 
     cheatcode:
@@ -118,3 +248,4 @@ void AllOrNothing::on_draw_ui() {
   ImGui::SliderInt("##StlyeRankRequirement", (int*)&AllOrNothing::stylebar, 1, 7);
   
 }
+#endif
