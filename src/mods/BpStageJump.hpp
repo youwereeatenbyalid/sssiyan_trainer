767ac9000562f78f2b7f025bdf665183a11bb07a
The Hitchhiker <youwereeatenbyalid@gmail.com>
2021-02-14 17:50:17 -0500

    New Randomizer + Boss rush options

--- src/mods/BpStageJump.hpp
+++ src/mods/BpStageJump.hpp
@@ -5,6 +5,28 @@ class BpStageJump : public Mod {
 public:
   BpStageJump() = default;
   // mod name string for config
+
+  
+  static bool randombosses;
+  static bool bossrush;
+  static bool endless;
+  static enum palace_type_enum { BALANCED, PARTIAL, RANDOM };
+  int static palace_type;
+  int static random_generator(int low, int high);
+  // random_generator(1,7), case switch to return a boss floor
+  int static return_boss_floor();
+  // return non-boss bp floor
+  int static return_normal_floor();
+  // Reset counter, palacearray,bossarray
+  void static reset_palace();
+  // randomize values in an array via swapping
+  void static randomize_array(int* array_param, int range_low, int range_high, int rand_low, int rand_high);
+  void static randomize_array(int* array_param, int range_low, int range_high);
+  // Generate a new palace scenario
+  void static generate_palace(int seed);
+  // get the next floor
+  int static next_floor();
+
   std::string_view get_name() const override { return "BpStageJump"; }
   std::string get_checkbox_name() override { return m_check_box_name; };
   std::string get_hotkey_name() override { return m_hot_key_name; };
@@ -24,12 +46,14 @@ public:
   // you are in the imgui window here.
   void on_draw_ui() override;
   // on_draw_debug_ui() is called when debug window shows up
-  // void on_draw_debug_ui() override;
+  void on_draw_debug_ui() override;
 
 private:
   // function hook instance for our detour, convinient wrapper
   // around minhook
   void init_check_box_info() override;
-
+  static int palacearray[100];
+  static int bossarray[8];
+  static int counter;
   std::unique_ptr<FunctionHook> m_function_hook;
 };
