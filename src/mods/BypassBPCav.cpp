5fda3ae902d441d1e276897dac0adcfa9b9c3a6d
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2022-02-18 17:42:08 -0500

    Bypass BP Cavaliere update
    skips pop-up

--- src/mods/BypassBPCav.cpp
+++ src/mods/BypassBPCav.cpp
@@ -1,35 +1,51 @@
 #include "BypassBPCav.hpp"
-#include "PlayerTracker.hpp"
-uintptr_t BypassBPCav::jmp_ret{NULL};
-uintptr_t BypassBPCav::jmp_jb{NULL};
+#include "mods/PlayerTracker.hpp"
+uintptr_t BypassBPCav::jmp_cavrfix1_return{NULL};
+uintptr_t BypassBPCav::jmp_cavrfix2_return{NULL};
 bool BypassBPCav::cheaton{NULL};
-
+//variables
 // clang-format off
 // only in clang/icl mode on x64, sorry
 
-static naked void detour() {
-	__asm {
-        // cmp [PlayerTracker::playerid], 1 // "dante" player isn't active when this is called
-        // jne code
-        cmp byte ptr [BypassBPCav::cheaton], 1
-        je cheatcode
-        jmp code
+  static naked void newmem_detour1() {
+__asm {
+  validation:
+    cmp byte ptr [BypassBPCav::cheaton], 1
+    je cheatcode
+    jmp code
+  code:
+    mov rax,[rbx+0x50]
+    mov rcx,[rax+0x18]
+    jmp qword ptr [BypassBPCav::jmp_cavrfix1_return]
 
-        cheatcode:
-		jmp qword ptr [BypassBPCav::jmp_ret]
+  cheatcode:
+    mov edx, 0x0
+    mov rax,[rbx+0x50]
+    mov rcx,[rax+0x18]
+    jmp qword ptr [BypassBPCav::jmp_cavrfix1_return]
+  }
+}
+  static naked void newmem_detour2() {
+__asm {
+  validation:
+    cmp byte ptr [BypassBPCav::cheaton], 1
+    je cheatcode
+    jmp code
+  code:
+    mov rax,[rbx+0x50]
+    mov rcx,[rax+0x18]
+    jmp qword ptr [BypassBPCav::jmp_cavrfix2_return]
 
-        code:
-        cmp esi, [rax+1Ch]
-        jb jbcode
-        jmp qword ptr [BypassBPCav::jmp_ret]
-
-        jbcode:
-        jmp qword ptr [BypassBPCav::jmp_jb]
-	}
+  cheatcode:
+    mov edx, 0x0
+    mov rax,[rbx+0x50]
+    mov rcx,[rax+0x18]
+    jmp qword ptr [BypassBPCav::jmp_cavrfix2_return]
+  }
 }
 
-// clang-format on
 
+// clang-format on
 void BypassBPCav::init_check_box_info() {
   m_check_box_name = m_prefix_check_box_name + std::string(get_name());
   m_hot_key_name   = m_prefix_hot_key_name + std::string(get_name());
@@ -38,29 +54,50 @@ void BypassBPCav::init_check_box_info() {
 std::optional<std::string> BypassBPCav::on_initialize() {
   init_check_box_info();
 
-  m_is_enabled          = &BypassBPCav::cheaton;
-  m_on_page             = bloodypalace;
-
-  m_full_name_string   = "Bypass BP Cavaliere Restriction";
-  m_author_string      = "SSSiyan";
+  auto base = g_framework->get_module().as<HMODULE>(); // note HMODULE
+  m_is_enabled = &BypassBPCav::cheaton;
+  m_on_page    = bloodypalace;
+  m_full_name_string = "Bypass BP Cavaliere Restriction";
+  m_author_string = "SSSiyan";
   m_description_string = "Allows you to take Cavaliere R into Bloody Palace.";
 
   set_up_hotkey();
 
-  auto base = g_framework->get_module().as<HMODULE>(); // note HMODULE
-  auto addr      = patterns->find_addr(base, "C8 00 EB 44 3B 70 1C 72 11");
-  if (!addr) {
-    return "Unable to find BypassBPCav pattern.";
-  }
-    
-   BypassBPCav::jmp_jb = patterns->find_addr(base, "42 89 0C 30 EB 28").value();
+  auto cavrfix1_addr = utility::scan(base, "1F 61 00 0F B6 D0 48 8B 43 50 48 8B 48 18");
 
-  if (!install_hook_absolute(addr.value()+4, m_function_hook, &detour, &jmp_ret, 5)) {
-    //  return a error string in case something goes wrong
-    spdlog::error("[{}] failed to initialize", get_name());
-    return "Failed to initialize BypassBPCav";
+  if (!cavrfix1_addr) {
+    return "Unable to find cavrfix1 pattern.";
   }
+  if (!install_hook_absolute(cavrfix1_addr.value()+0x06, m_cavrfix1_hook, &newmem_detour1, &jmp_cavrfix1_return, 8)) {
+    //return a error string in case something goes wrong
+    spdlog::error("[{}] failed to initialize", get_name());
+    return "Failed to initialize cavrfix1";
+  }
+  
+  
+  auto cavrfix2_addr = utility::scan(base, "96 60 00 0F B6 D0 48 8B 43 50 48 8B 48 18");
+
+  if (!cavrfix2_addr) {
+    return "Unable to find cavrfix2 pattern.";
+  }
+  if (!install_hook_absolute(cavrfix2_addr.value()+0x06, m_cavrfix2_hook, &newmem_detour2, &jmp_cavrfix2_return, 8)) {
+    //return a error string in case something goes wrong
+    spdlog::error("[{}] failed to initialize", get_name());
+    return "Failed to initialize cavrfix2";
+  }
+  
+  
+
   return Mod::on_initialize();
 }
 
-// void BypassBPCav::on_draw_ui() {}
+// during load
+//void BypassBPCav::on_config_load(const utility::Config &cfg) {}
+// during save
+//void BypassBPCav::on_config_save(utility::Config &cfg) {}
+// do something every frame
+//void BypassBPCav::on_frame() {}
+// will show up in debug window, dump ImGui widgets you want here
+//void BypassBPCav::on_draw_debug_ui() {}
+// will show up in main window, dump ImGui widgets you want here
+//void BypassBPCav::on_draw_ui() {}
