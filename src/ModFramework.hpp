5a2ab61183f0738da3dd0f1c012875353a536b50
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2023-04-12 15:52:18 -0400

    Update version names

--- src/ModFramework.hpp
+++ src/ModFramework.hpp
@@ -94,7 +94,7 @@ private:
     enum PanelID_ : uint8_t;
     enum OptionID_ : uint8_t;
 
-    static constexpr char TRAINER_VERSION_STR[7] = "v1.23b";
+    static constexpr char TRAINER_VERSION_STR[7] = "v1.23c";
 
     void draw_ui();
     void draw_panels() const;
