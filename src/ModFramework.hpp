84def7a926932b189288445cfc35237a900ea5b5
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2023-06-13 18:42:56 -0400

    Version update
    because dank was a little bitch and cba to do it

--- src/ModFramework.hpp
+++ src/ModFramework.hpp
@@ -103,7 +103,7 @@ private:
     enum PanelID_ : uint8_t;
     enum OptionID_ : uint8_t;
 
-    static constexpr char TRAINER_VERSION_STR[7] = "v1.24b";
+    static constexpr char TRAINER_VERSION_STR[7] = "v1.24c";
 
     void draw_ui();
     void draw_panels() const;
