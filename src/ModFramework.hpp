733b7ee491f37f2935b99345b2b0f035c5ba9500
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2023-08-02 14:14:34 -0400

    Version increment

--- src/ModFramework.hpp
+++ src/ModFramework.hpp
@@ -106,7 +106,7 @@ private:
     enum PanelID_ : uint8_t;
     enum OptionID_ : uint8_t;
 
-    static constexpr char TRAINER_VERSION_STR[7] = "v1.24e";
+    static constexpr char TRAINER_VERSION_STR[7] = "v1.24f";
 
     void initialize_game_specifics();
 
