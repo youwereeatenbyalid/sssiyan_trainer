9799aa41f3923e3d90c82edd76d441b5b2ec384f
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2023-08-03 14:25:37 -0400

    add damage modifier as a pvp dependency to fix player health

--- src/mods/HeavyDay.cpp
+++ src/mods/HeavyDay.cpp
@@ -703,7 +703,7 @@ std::optional<std::string> HeavyDay::on_initialize() {
   auto base              = g_framework->get_module().as<HMODULE>(); // note HMODULE
   m_is_enabled           = &HeavyDay::cheaton;
   m_on_page              = Page_GameMode;
-  m_depends_on           = { "PlayerTracker" };
+  m_depends_on           = { "PlayerTracker","AllOrNothing"};
   m_full_name_string     = "PVP";
   m_author_string        = "The HitchHiker, SSSiyan, Dr. Penguin";
   m_description_string   = "Enables PVP Combat between players."
